aa04cb67e990065cb9be20ccfb17f9c31440dd66
Reginaldo Junior <76regi@gmail.com>
2019-03-27 16:13:29 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,9 +1,11 @@
 const MONGO_HOST = process.env.MONGO_HOST || '127.0.0.1';
 const MONGO_PORT = process.env.MONGO_PORT || '27017';
 const MONGO_DB = process.env.MONGO_DB || 'certificados';
+const MONGO_USER = process.env.MONGO_USER || 'root';
+const MONGO_PASS = process.env.MONGO_PASS || '';
 
 const mongoose = require('mongoose');
-const strConnectionMongo = `mongodb://${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}`;
+const strConnectionMongo = `mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}`;
 
 mongoose.connect(strConnectionMongo, { useNewUrlParser: true });
 
