aa9d6a6cfcfeb19e25a8042aabbd605a222de563
Reginaldo Junior <reginaldo.junior@creditas.com>
2025-01-27 13:53:41 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -3,10 +3,15 @@ const app = express();
 const bodyParser = require('body-parser');
 const router = express.Router();
 const database = require('../config/database');
+const cors = require('cors');
 
 //Rotas
 const index = require('./routes/index');
 const certified = require('./routes/certifiedRoute');
+
+app.use(cors({
+    origin: ['http://localhost:4000', 'https://quebra.dev']
+}))
 
 app.use(bodyParser());
 
